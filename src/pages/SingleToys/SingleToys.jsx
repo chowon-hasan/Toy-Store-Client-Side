6f5a47976d7deb9781fc8abc1426eb394ed59e91
Chowon-Hasan <chowonhasan7@gmail.com>
2023-05-20 14:20:06 +0600

    completed single toys page

--- src/pages/SingleToys/SingleToys.jsx
+++ src/pages/SingleToys/SingleToys.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Rating from "react-rating";
+import { FaRegStar, FaStar, FaStarHalf } from "react-icons/fa";
 
 const SingleToys = () => {
   const singleToys = useLoaderData();
@@ -17,10 +19,45 @@ const SingleToys = () => {
   } = singleToys;
   console.log(singleToys);
   return (
-    <section>
-      <div className="xl:container mx-auto">
-        <div className="lg:w-1/2 sm:w-full mx-auto">
-          <h1>{toyName}</h1>
+    <section className="bg-white py-24">
+      <div className="xl:container mx-auto bg-white">
+        <div className="lg:w-1/2 sm:w-full mx-auto border p-5">
+          <h1 className="text-black">
+            Toy Name :{" "}
+            <span className="text-yellow-500 text-2xl">{toyName}</span>
+          </h1>
+          <div className="flex justify-center">
+            <img className="h-96" src={image} alt="" />
+          </div>
+          <div className="flex justify-between">
+            <h1 className="text-black">
+              Price : $<span className="text-yellow-500">{price}</span>
+            </h1>
+            <div className="text-yellow-500 flex ">
+              <h1 className="mr-5 text-black">Rating</h1>
+              <Rating
+                placeholderRating={rating}
+                emptySymbol={<FaRegStar />}
+                placeholderSymbol={<FaStar />}
+                fullSymbol={<FaStarHalf />}
+                readonly
+              />
+            </div>
+            <h1 className="text-black">
+              Quantity : <span className="text-yellow-500">{quantity}</span>
+            </h1>
+          </div>
+          <div className="flex justify-between my-5">
+            <h1 className="text-black">
+              Seller Email : <span className="text-yellow-500">{email}</span>
+            </h1>
+            <h1 className="text-black">
+              Seller : <span className="text-yellow-500">{seller}</span>
+            </h1>
+          </div>
+          <h1 className="text-black">
+            Description : <p className="mt-2">{description}</p>
+          </h1>
         </div>
       </div>
     </section>
